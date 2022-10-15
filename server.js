e689b7c4d5d6399c54c359504ba0b0a5d57eb935
jhansi <jhansikongara216@gmail.com>
2022-10-15 18:54:26 +0530

    initial commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var cors = require("cors");
 app.use(cors());
 let AuthRoute = require("./routers/authRouter");
 const PORT = process.env.PORT || 4000;
-app.use("", express.static(__dirname + "/dista/grocery/"));
+app.use(express.static(__dirname + "/dista/grocery/"));
 app.use("/api", AuthRoute);
 app.listen(PORT, () => {
   console.log("server Running on Port Number: " + PORT);
