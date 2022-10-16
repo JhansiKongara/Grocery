2b587f4adc25627325a92315971d65160955aa67
jhansi <jhansikongara216@gmail.com>
2022-10-16 09:21:37 +0530

    fixing

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 let express = require("express");
 let path = require("path");
+const config = require("config");
 let bodyParser = require("body-parser");
 require("./dbConnection/db");
 require("dotenv").config();
@@ -7,8 +8,10 @@ let app = express();
 app.use(bodyParser.json());
 var cors = require("cors");
 app.use(cors());
+console.log(config.get("Customer.dbConfig"));
 let AuthRoute = require("./routers/authRouter");
 const PORT = process.env.PORT || 4000;
+console.log(process.env);
 app.use(express.static(path.join(__dirname, "dist/grocery")));
 app.use("/api", AuthRoute);
 app.get("/*", (req, res) => {
