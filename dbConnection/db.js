98eb8358fbb80967aa23676441938ba209fbb5ee
jhansi <jhansikongara216@gmail.com>
2022-10-16 13:06:49 +0530

    fixing

--- dbConnection/db.js
+++ dbConnection/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const config = require("config");
+console.log(config.get("Customer.dbConfig.db_url"), process.env.NODE_ENV);
 mongoose
   .connect(config.get("Customer.dbConfig.db_url"), {
     useNewUrlParser: true,
